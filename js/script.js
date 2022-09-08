65a8b2e9388379c13493486a4f016aad9f9d8b5b
Rowa <jrowah@gmail.com>
2022-09-08 22:14:53 +0300

    filter method

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const accountNames = [
+const accounts = [
   { accountName: "OptimisticPanda", noOfFollowers: 100 },
   { accountName: "SunshineSoul", noOfFollowers: 200 },
   { accountName: "GratefulHeart", noOfFollowers: 150 },
@@ -10,7 +10,31 @@ const accountNames = [
 ];
 
 //Filter Method
-const lessThan500Followers = accountNames.filter((account) => {
+const lessThan500Followers = accounts.filter((account) => {
   return account.noOfFollowers < 500;
 });
 console.log(lessThan500Followers);
+// [
+//   {
+//     accountName: 'OptimisticPanda',
+//     noOfFollowers: 100
+//   },
+//   {
+//     accountName: 'SunshineSoul',
+//     noOfFollowers: 200
+//   },
+//   {
+//     accountName: 'GratefulHeart',
+//     noOfFollowers: 150
+//   },
+//   {
+//     accountName: 'DynamicDiva',
+//     noOfFollowers: 300
+//   },
+//   {
+//     accountName: 'PeacefulPlace',
+//     noOfFollowers: 426
+//   }
+// ]
+//logging accounts gives the original array
+console.log(accounts);
