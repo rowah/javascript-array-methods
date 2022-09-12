b1b01c1720cdb7ff474d5afc30d1910f361d7773
Rowa <jrowah@gmail.com>
2022-09-12 17:08:48 +0300

    splice method

--- js/script.js
+++ js/script.js
@@ -68,6 +68,7 @@ accounts.forEach((account) => {
 // 'Rodenta'
 
 //SOME METHOD
+//tests whether at least one element in the array passes the test implemented in the testing function and return true or false
 const zeroFollowers = accounts.some((account) => {
   return account.noOfFollowers == 0;
 });
@@ -75,6 +76,7 @@ const zeroFollowers = accounts.some((account) => {
 console.log(zeroFollowers); //false
 
 //EVERY METHOD
+//works the same as some method except that it checks if every element in the array passes the test implemented in the testing function and if they all do, it returns true
 console.log(
   accounts.every((account) => {
     return account.noOfFollowers >= 100;
@@ -82,6 +84,7 @@ console.log(
 ); //returns true
 
 //REDUCE METHOD
+//Performs reduction
 const allFollowers = accounts.reduce(
   (previousTotal, currentAccount) =>
     previousTotal + currentAccount.noOfFollowers,
@@ -90,5 +93,48 @@ const allFollowers = accounts.reduce(
 console.log(allFollowers); // 70877
 
 //INCLUDES METHOD
+//determines if an array includes a certain value and returns either true or false depending on the availability of that element
 const followers = [100, 400, 130, 300, 1200, 56000, 426, 12301];
-console.log(followers.includes(12301));
+console.log(followers.includes(12301)); //true
+
+//SPLICE METHOD
+//The splice method deletes, inserts, and/or replaces elements in an array
+//Deletion
+// accounts.splice(1, 1);
+// console.log(accounts);
+
+//Replacement
+// const deletedAccounts = accounts.splice(
+//   1,
+//   2,
+//   { accountName: "Tumbulu", noOfFollowers: 450 },
+//   { accountName: "Bondo", noOfFollowers: 340 }
+// );
+// console.log(accounts);
+// 0: {accountName: 'OptimisticPanda', noOfFollowers: 100}
+// 1: {accountName: 'Tumbulu', noOfFollowers: 450}
+// 2: {accountName: 'Bondo', noOfFollowers: 340}
+// 3: {accountName: 'DynamicDiva', noOfFollowers: 300}
+// 4: {accountName: 'SassyStarlet', noOfFollowers: 1200}
+// 5: {accountName: 'BelieveInYou', noOfFollowers: 56000}
+// 6: {accountName: 'PeacefulPlace', noOfFollowers: 426}
+// 7: {accountName: 'Rodenta', noOfFollowers: 12301}
+//console.log(deletedAccounts);
+//0: {accountName: 'SunshineSoul', noOfFollowers: 400}
+//1: {accountName: 'GratefulHeart', noOfFollowers: 150}
+
+//Insertion
+// from index 3
+// delete 0
+// then insert {accountName: 'Royman', noOfFollowers: 5000}
+// accounts.splice(3, 0, { accountName: "Royman", noOfFollowers: 5000 });
+// console.log(accounts);
+// 0: {accountName: 'OptimisticPanda', noOfFollowers: 100}
+// 1: {accountName: 'SunshineSoul', noOfFollowers: 400}
+// 2: {accountName: 'GratefulHeart', noOfFollowers: 150}
+// 3: {accountName: 'Royman', noOfFollowers: 5000}
+// 4: {accountName: 'DynamicDiva', noOfFollowers: 300}
+// 5: {accountName: 'SassyStarlet', noOfFollowers: 1200}
+// 6: {accountName: 'BelieveInYou', noOfFollowers: 56000}
+// 7: {accountName: 'PeacefulPlace', noOfFollowers: 426}
+// 8: {accountName: 'Rodenta', noOfFollowers: 12301}
